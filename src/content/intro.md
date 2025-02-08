f96ead8a7b35427463783addf4f9bfceca128725
Shreyas Kapur <shreyaskapur8@gmail.com>
2025-02-08 15:52:30 -0800

    Markdownify.

---
+++ src/content/intro.md
@@ -0,0 +1,7 @@
+This workshop explores the emerging paradigm of using programmatic representations to enhance
+the interpretability, generalizability, efficiency, and scalability of frameworks for agent
+learning and decision-making. The goal of this workshop is to bring together the sequential
+decision- making community, which includes robot learning, robotics, reinforcement learning,
+imitation learning, planning, search, and optimal control, with the code generation and program
+synthesis communities to confront the challenges in agent and robot learning at scale and solve
+specific applications ranging from virtual agents to robotics.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,8 @@
 	import ArxivIcon from 'virtual:icons/simple-icons/arxiv';
 	import Person from '$lib/components/Person.svelte';
 	import NavLink from '$lib/components/NavLink.svelte';
-	import Call from '$lib/call.md';
+	import Call from '../content/call.md';
+	import Intro from '../content/intro.md';
 </script>
 
 <svelte:head>
@@ -49,13 +50,7 @@
 		</div>
 	</div> -->
 	<Block padding="py-8" class="md:text-justify">
-		This workshop explores the emerging paradigm of using programmatic representations to enhance
-		the interpretability, generalizability, efficiency, and scalability of frameworks for agent
-		learning and decision-making. The goal of this workshop is to bring together the sequential
-		decision- making community, which includes robot learning, robotics, reinforcement learning,
-		imitation learning, planning, search, and optimal control, with the code generation and program
-		synthesis communities to confront the challenges in agent and robot learning at scale and solve
-		specific applications ranging from virtual agents to robotics.
+		<Intro />
 	</Block>
 	<Block>
 		<h1 class="mb-6 text-2xl font-bold" id="schedule">Tentative Schedule</h1>
